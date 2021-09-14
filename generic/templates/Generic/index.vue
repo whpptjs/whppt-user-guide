c2d6a73b00bd2e79ed72e485c856070f41bd9af8
Ethan Sun <sunnysun9312@gmail.com>
2021-09-14 20:31:37 +0930

    Update index.vue

--- generic/templates/Generic/index.vue
+++ generic/templates/Generic/index.vue
@@ -3,13 +3,13 @@
     <div class="w-3/12 border-r border-gray-200">
       <PageNavigation />
     </div>
-    <div class="w-7/12 pt-8 nav-spacer">
+    <div class="w-7/12 pt-4 nav-spacer">
       <div v-whppt-text="page.header" data-property="title" class="font-extrabold text-3xl mb-8 container">
         {{ page.header.title || 'Title' }}
       </div>
       <w-content :content-items="page.contents" style="min-height: 200px" class="page-contents w-full" />
     </div>
-    <anchor-list class="w-2/12 pt-8 nav-spacer" :page="page" />
+    <anchor-list class="w-2/12 pt-4 nav-spacer" :page="page" />
   </div>
 </template>
 
