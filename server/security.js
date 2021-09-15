85f4f5017cc110cf53bd5b670b219a6605733507
Ethan Sun <sunnysun9312@gmail.com>
2021-09-15 16:22:35 +0930

    Update security.js

--- server/security.js
+++ server/security.js
@@ -2,7 +2,7 @@ module.exports = {
   provider: 'jwt',
   jwt: {
     secret: process.env.JWT_SECRET,
-    issuer: 'whppt-playground',
-    audience: 'whppt-playground.com',
+    issuer: 'whppt-user-guide',
+    audience: 'guide.whppt.org',
   },
 };
