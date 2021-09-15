0fe232260c9d0f6cb277e72d5afc0731f6b6fc3b
Jake Vandborg <jake.vandborg@gmail.com>
2021-09-15 11:18:09 +0930

    tall image and image captions

--- sharedComponents/components.js
+++ sharedComponents/components.js
@@ -1,5 +1,5 @@
 import { Components } from '@whppt/nuxt/lib/plugins/Components';
 
-import { imageComponent, youtube, blockContent } from './definitions/index';
+import { imageComponent, tallImageComponent, youtube, blockContent } from './definitions/index';
 
-export default [imageComponent, youtube, blockContent, ...Components];
+export default [imageComponent, tallImageComponent, youtube, blockContent, ...Components];
