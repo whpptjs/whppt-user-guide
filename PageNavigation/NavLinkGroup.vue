13e55842439b4254271a545ba1cc8b271eff4d69
Jake Vandborg <jake.vandborg@gmail.com>
2021-09-15 14:01:34 +0930

    reworked some of the page nav

--- PageNavigation/NavLinkGroup.vue
+++ PageNavigation/NavLinkGroup.vue
@@ -1,31 +1,40 @@
 <template>
   <div>
-    <button
-      class="
-        w-full
-        flex
-        justify-between
-        items-center
-        py-1
-        px-2
-        text-gray-500
-        hover:bg-gray-100
-        transition
-        duration-200
-        ease-in-out
-        rounded-sm
-      "
-      @click="$emit('openItem', itemIdx)"
-    >
-      <div>
-        {{ item.link.text || 'Link text' }}
-      </div>
-      <icon icon="ChevronDown" class="w-3 h-3 arrow" :class="subItemsOpen ? 'arrow-up' : 'arrow-down'" />
-    </button>
+    <div v-whppt-link="item.link">
+      <whppt-link v-if="item.link.href" :to="item.link">
+        <NavButton :item="item">{{ item.link.text || 'Link text' }}</NavButton>
+      </whppt-link>
+      <button
+        v-else
+        class="
+          w-full
+          flex
+          justify-between
+          items-center
+          py-1
+          px-2
+          text-gray-500
+          hover:bg-gray-100
+          transition
+          duration-200
+          ease-in-out
+          rounded-sm
+        "
+        @click="$emit('openItem', itemIdx)"
+      >
+        <div>
+          {{ item.link.text || 'Link text' }}
+        </div>
+        <icon icon="ChevronDown" class="w-3 h-3 arrow" :class="subItemsOpen ? 'arrow-up' : 'arrow-down'" />
+      </button>
+    </div>
+
     <div v-if="subItemsOpen" class="ml-2">
       <div v-whppt-list="{ data: item, addNew }" data-property="subItems" :class="{ 'py-4': inEditor }">
         <div v-for="(subItem, index) in item.subItems" :key="index" class="my-1">
-          <NavLink :item="subItem" />
+          <whppt-link v-whppt-link="subItem.link" :to="subItem.link">
+            <NavButton :item="subItem" />
+          </whppt-link>
         </div>
       </div>
     </div>
@@ -33,14 +42,14 @@
 </template>
 
 <script>
-import { mapState, mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
-import NavLink from './NavLink.vue';
+import NavButton from './NavButton.vue';
 
 export default {
   name: 'NavigationLinkGroup',
   components: {
-    NavLink,
+    NavButton,
   },
   props: {
     item: { type: Object, required: true },
@@ -48,7 +57,6 @@ export default {
     itemIdx: { type: Number, required: true },
   },
   computed: {
-    ...mapState('whppt/site', ['nav']),
     ...mapGetters(['inEditor']),
   },
   mounted() {
@@ -57,9 +65,6 @@ export default {
   },
   methods: {
     ...mapActions('whppt/editor', ['pushSelectedComponentState']),
-    setOpenItem(itemIdx) {
-      this.openItemIdx = this.openItemIdx === itemIdx ? undefined : itemIdx;
-    },
     addNew() {
       this.pushSelectedComponentState({
         path: 'subItems',
