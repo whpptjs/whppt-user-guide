94f76dc79ff5b9609dd9dae1de59388520dff607
Jake Vandborg <jake.vandborg@gmail.com>
2021-09-17 13:30:41 +0930

    optional home header description, better ratio for home header image

--- generic/headers/Home.vue
+++ generic/headers/Home.vue
@@ -4,19 +4,29 @@
       <div class="container py-12 text-white flex">
         <div class="w-1/12" />
         <div class="w-8/12">
-          <div v-whppt-text="header" data-property="title" class="text-3xl font-bold">
-            {{ header.title || 'Title' }}
-          </div>
-          <div class="flex mt-8">
-            <whppt-link
-              v-if="header.primaryLink.href || inEditor"
-              v-whppt-link="header.primaryLink"
-              :to="header.primaryLink"
+          <div class="mr-4">
+            <div v-whppt-text="header" data-property="title" class="text-3xl font-bold">
+              {{ header.title || 'Title' }}
+            </div>
+            <div
+              v-if="header.description || inEditor"
+              v-whppt-text="header"
+              data-property="description"
+              class="text-xl"
             >
-              <div class="bg-primary rounded-lg text-center font-bold px-8 py-2 text-lg">
-                {{ header.primaryLink.text || 'Primary button' }}
-              </div>
-            </whppt-link>
+              {{ header.description || 'description' }}
+            </div>
+            <div class="flex mt-8">
+              <whppt-link
+                v-if="header.primaryLink.href || inEditor"
+                v-whppt-link="header.primaryLink"
+                :to="header.primaryLink"
+              >
+                <div class="bg-primary rounded-lg text-center font-bold px-8 py-2 text-lg">
+                  {{ header.primaryLink.text || 'Primary button' }}
+                </div>
+              </whppt-link>
+            </div>
           </div>
         </div>
         <div class="w-2/12">
@@ -25,18 +35,18 @@
             :data-sizes="
               JSON.stringify({
                 desktop: {
-                  width: 900,
-                  height: 900,
+                  width: 1024,
+                  height: 768,
                   quality: 2,
                 },
               })
             "
-            class="aspect-ratio-square relative"
+            class="aspect-ratio-4/3 relative"
             :class="{ 'bg-gray-200': !header.image.imageId || inEditor }"
           >
             <img
               v-if="header.image.imageId"
-              v-lazy="$whppt.getImage(header.image.imageId, 900, 900, { crop: header.image.desktop })"
+              v-lazy="$whppt.getImage(header.image.imageId, 1024, 900, { crop: header.image.desktop })"
               class="absolute inset-0 w-full h-full"
             />
           </div>
